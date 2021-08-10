4447b5400fd83e226aff638089d1233fe4a6d1a9
Tomás Ferreyra <tomaslicenciado@gmail.com>
2021-08-10 20:53:25 -0300

    App products primera etapa

--- api_e_comerce/products/admin.py
+++ api_e_comerce/products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Product, Category, Supplier
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'unit_price', 'supplier')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category)
+admin.site.register(Supplier)
